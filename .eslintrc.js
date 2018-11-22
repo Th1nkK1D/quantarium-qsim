72622ab984fbf30a20bda2d2a11a9a27669beca5
Withee Poositasai <witheep@gmail.com>
2018-11-22 12:02:50 +0700

    Setup project structure, add eslint and babel hook

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es6": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      4
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ],
+    "no-console": [
+      "error",
+      {
+        "allow": ["log"]
+      }
+    ]
+  }
+};
---
+++ index.js
@@ -0,0 +1,3 @@
+require('@babel/register')
+// require the rest of the app that needs to be transpiled after the hook
+require('./src/main')
