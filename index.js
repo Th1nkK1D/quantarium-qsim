a14a0e178f4f44baa3812c8215b4b057c276de2d
Withee Poositasai <witheep@gmail.com>
2018-11-25 20:46:06 +0700

    Set up babel build

--- index.js
+++
@@ -1,5 +0,0 @@
-import Qubit from './src/qubit'
-import Gate from './src/gate'
-import BasicGate from './src/basicGate'
-
-export { Qubit, Gate, BasicGate }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Qubit from './qubit'
+import Gate from './gate'
+import BasicGate from './basicGate'
+
+export { Qubit, Gate, BasicGate }
